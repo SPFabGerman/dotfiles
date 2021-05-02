9908dfdab7491e6b77e89975ef48308d6290fa0a
SPFabGerman <42518661+SPFabGerman@users.noreply.github.com>
2021-05-02 16:38:06 +0200

    added submodules

--- .config/polybar/pulseaudio_wrapper.sh
+++ .config/polybar/pulseaudio_wrapper.sh
@@ -4,7 +4,7 @@ COLORMUTE="$(xrdb -query | awk '/\*.color8:/{print $2}' | sed 's/#//')"
 export COLORBOX="$(xrdb -query | awk '/alpha_color3:/{print $2}')"
 export COLORRESET="$(xrdb -query | awk '/alpha_color15:/{print $2}')"
 
-polybar-pulseaudio-control \
+pulseaudio-control \
 	--format '%{B$COLORBOX} %{T2}$SINK_NICKNAME%{T-} %{B$COLORRESET} ${VOL_LEVEL}% %{B-}' \
 	--color-muted "$COLORMUTE" \
 	--sink-nickname "alsa_output.pci-0000_00_1f.3.analog-stereo":"墳" \
---
+++ bin/colormake.pl
@@ -0,0 +1,1 @@
+../build/Colormake/colormake.pl
