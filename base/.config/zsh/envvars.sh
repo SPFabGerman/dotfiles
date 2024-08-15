31724ef4b7c972f0a076e956d5099a73bf094a8a
SPFab <42518661+SPFabGerman@users.noreply.github.com>
2024-08-15 16:59:17 +0200

    Small update for gopath

--- base/.config/zsh/envvars.sh
+++ base/.config/zsh/envvars.sh
@@ -76,6 +76,7 @@ export JUPYTER_CONFIG_DIR="$XDG_CONFIG_HOME/jupyter"
 export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME/java"
 export MPLAYER_HOME="$XDG_CONFIG_HOME/mplayer"
 export GOPATH="$XDG_DATA_HOME/go"
+export GOMODCACHE="$XDG_CACHE_HOME/go/mod"
 isInstalled go && ( [[ $PATH =~ $(go env GOPATH)/bin ]] || PATH="$(go env GOPATH)/bin:$PATH" )
 export NODE_REPL_HISTORY="$XDG_DATA_HOME/node_repl_history"
 export KDEHOME="$XDG_CONFIG_HOME/kde"
